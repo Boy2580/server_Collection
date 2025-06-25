81c2d3450d7c558f73d7c67e671ee24d30158adb
Boyxam <Boycubu@gmail.com>
2025-06-25 12:48:04 +0700

    update Skill

--- controller/Skill.js
+++ controller/Skill.js
@@ -77,7 +77,28 @@ exports.GetSkills = async (req, res) => {
 
 exports.UpdateSkill = async (req, res) => {
     try {
+        const { userId,skillId } = req.params;
+        const { name, description, level, images } = req.body;
 
+        const updateSkill = await prisma.skill.update({
+            where:{
+                id: Number(skillId),
+                userId: Number(userId) // Assuming you have a userId field in your skill model
+            },
+            data:{
+                ...name && { name: name },
+                ...description && { description: description },
+                ...level && { level: level },
+                ...images && {
+                    images: {
+                        create: images.map((file) => ({
+                            imageUrl: file.url, // Assuming file.path contains the URL of the uploaded image
+                            assetId: file.asset_id, // Assuming file.asset_id contains the asset ID from your storage service
+                            publicId: file.public_id
+                        }))
+                    }
+            }}
+        })
 
         res.status(200).json({ message: 'Skill updated successfully' }); // Replace with actual updated skill data
     }
@@ -88,9 +109,15 @@ exports.UpdateSkill = async (req, res) => {
 
 exports.DeleteSkill = async (req, res) => {
     try {
+    const { userId, skillId } = req.params;
 
-
-        res.status(200).json({ message: 'Skill deleted successfully' }); // Replace with actual deletion logic  
+    const skill = await prisma.skill.delete({
+        where: { 
+            userId: Number(userId), // Assuming you have a userId field in your skill model
+            id: Number(skillId) // Assuming you have a skillId field in your skill model
+         }
+        })
+        res.status(200).json({ message: 'Skill deleted successfully' }); // Replace with actual deletion logic 
     }
     catch (error) {
         return res.status(500).json({ message: 'Server error', error: error.message });
